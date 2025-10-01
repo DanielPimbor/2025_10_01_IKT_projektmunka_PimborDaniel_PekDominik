1d19b8208afa85b21915e62dbadfacc2f311cdd2
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2025-10-01 10:14:33 +0200

    Kopapirollo update

---
+++ kopapirollo.py
@@ -0,0 +1,16 @@
+valasz = input('Köszöntelek a "Kő, papír, olló" játékban. Szeretnél játszani? i/n : ')
+
+while True:
+    if valasz == 'i':
+        print('Örülök, hogy így döntöttél!')
+        break
+
+    elif valasz == 'n':
+        print('Pápá!')
+        break
+
+    else:
+        valasz = input('Nem megfelelő válasz próbáld újra: ')
+
+
+print('alma')
