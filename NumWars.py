a921ed17afd2b5634feb63ad6f33970524859687
PekDominim <pek.dominik@diak.szbi-pg.hu>
2025-10-07 19:45:33 +0200

    Finished the project
    Added better UI

--- NumWars.py
+++ NumWars.py
@@ -1,42 +1,77 @@
 import random
 import sys
 
-print("Üdv a Számháború játékban!")
-
-
-
-while True:
-    num = int(input("Adj meg egy egész számot 1 és 6 között "))
-
-    if 6 >= num >= 1:
-        break
-
-    else:
-        print("Csak 1 és 6 közötti számot adhatsz meg!")
-
-print(f'{num}')
-num2 = random.randint(1,6)
-
 Nyert=0
 Vesztett=0
 Döntetlen=0
-
-if num > num2:
-    print("Nyertél")
-    Nyert=+1
-
-elif num < num2:
-    print("Vesztettél")
-    Vesztett=+1
-
-else:
-    print("Döntetlen!")
-    Döntetlen=+1
-
-print(f"Nyert {Nyert}")
-print(f"Vesztett {Vesztett}")
-print(f"Döntetlen {Döntetlen}")
+print("")
+print("Üdv a Számháború játékban!")
+print("")
+print("---------------------------------------")
+print("")
 
 
+while True:
+    while True:
+        num = int(input("Adj meg egy egész számot 1 és 6 között "))
+        print("")
+        print("---------------------------------------")
+
+        if 6 >= num >= 1:
+            break
+
+        else:
+            print("")
+            print("Csak 1 és 6 közötti számot adhatsz meg!")
+            print("")
+            print("---------------------------------------")
+
+    num2 = random.randint(1,6)
+
+    print(f'Gép: {num2}')
+    print("")
+
+    
+    if num > num2:
+        print("\033[32mNyertél!\033[0m")   
+        Nyert+=1
+
+    elif num < num2:
+        print("\033[31mVesztettél!\033[0m")
+        Vesztett+=1
+
+    else:
+        print("\033[33mDöntetlen!\033[0m")
+        Döntetlen+=1
+    print("---------------------------------------")
+    print(f"\033[32mNyert {Nyert}\033[0m")
+    print(f"\033[31mVesztett {Vesztett}\033[0m")
+    print(f"\033[33mDöntetlen {Döntetlen}\033[0m")
+    print("---------------------------------------")
+    while True:
+        print("")
+        yn = input("Szeretnél újra játszani? (y/n) ")
+        print("")
+        print("---------------------------------------")
+        if yn == "y":
+            break
+        elif yn == "n":
+            break
+        else:
+            print("Próbáld újra")
+    if yn =="n":
+        break
+print("Végeredmény:")
+print(f"\033[32mNyert {Nyert}\033[0m")
+print(f"\033[31mVesztett {Vesztett}\033[0m")
+print(f"\033[33mDöntetlen {Döntetlen}\033[0m")
+print("Köszönöm a játékot")
+print("---------------------------------------")
+
+
+"""
+Forrás a színes szöveghez
+https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+"""
 
 
